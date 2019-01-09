0fac9516a645713ce03e8d233c8157bc93f4363a
Nagma Yasmin <nyasmin@esri.com>
2019-01-08 17:48:59 -0800

    Triggers added

---
+++ XamarinBehaviors/XamarinBehaviors/LabelBindableProperty/AltLabel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinBehaviors.LabelBindableProperty
+{
+   public class AltLabel : Label
+    {
+        public static BindableProperty TextChangedProperty = BindableProperty.Create(nameof(TextChanged), typeof(string), typeof(AltLabel), string.Empty, propertyChanged: OnTextChanged);
+
+        
+        public string TextChanged
+        {
+            get { return (string)GetValue(TextChangedProperty); }
+            set { SetValue(TextChangedProperty, value); }
+        }
+
+        static void OnTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ((AltLabel)bindable).OnTextChanged((string)oldValue, (string)newValue);
+            
+        }
+
+        private void OnTextChanged(string oldValue, string newValue)
+        {
+            SetLabelText(newValue);
+           // bool isChanged = oldValue == newValue ? true : false;
+        }
+
+        private void SetLabelText(string newValue)
+        {
+            Text = newValue;
+        }
+    }
+}
---
+++ XamarinBehaviors/XamarinBehaviors/Triggers/TextChangedTriggerAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using XamarinBehaviors.LabelBindableProperty;
+
+namespace XamarinBehaviors.Triggers
+{
+    public class TextChangedTriggerAction : TriggerAction<AltLabel>
+    {
+        protected override void Invoke(AltLabel sender)
+        {
+            string oldScoreValue = ((AltLabel)sender).Text;
+         //   bool isChanged = oldScoreValue == 
+        }
+    }
+}
